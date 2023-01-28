6ccb3a29974b33ab82aa95c04abb56d5017feb0b
mohammadpiriyan <50291430+mohammadpiriyan@users.noreply.github.com>
2023-01-28 15:37:54 +0330

    creat function top salary

---
+++ q1/script.js
@@ -0,0 +1,12 @@
+let salaries = {
+  John: 100,
+  Pete: 300,
+  Mary: 250,
+};
+let list;
+list = Object.entries(salaries);
+function topsalary() {
+  console.log(list);
+  return list;
+}
+console.log(list);
