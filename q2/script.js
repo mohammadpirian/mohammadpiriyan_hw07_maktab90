cc22eae998ec6a00299eb9ead8a27faac00a16a0
mohammadpiriyan <50291430+mohammadpiriyan@users.noreply.github.com>
2023-01-28 18:05:55 +0330

    complete sort list func

--- q2/script.js
+++ q2/script.js
@@ -1,7 +1,23 @@
-let list=[[12,235,23,65],[127,85,53,75,5],[2,23,4,1,90,94,3]]
-function sortlist(a){
-    let arr=[]
-    for(let i=0;i<a.length;i++){
-        
+let list = [
+  [12, 235, 23, 65],
+  [127, 85, 53, 75, 5],
+  [2, 23, 4, 1, 90, 94, 3],
+];
+
+function sortlist(a) {
+  let arr = [];
+
+  for (let i = 0; i < a.length; i++) {
+    if (Array.isArray(a[i])) {
+      arr = arr.concat(sortlist(a[i]));
+    } else {
+      arr.push(a[i]);
     }
+  }
+  return typeof arr[0] === "string"
+    ? [...new Set(arr)].sort()
+    : [...new Set(arr)].sort((num1, num2) => {
+        return num1 - num2;
+      });
 }
+console.log(sortlist(list));
